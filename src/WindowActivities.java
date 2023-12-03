3152e88744c2da6088b1d56c33826c96f7c92743
Yoisy <yoisyp@gmail.com>
2023-12-02 22:19:57 -0600

    test

--- src/WindowActivities.java
+++ src/WindowActivities.java
@@ -13,7 +13,7 @@ public class WindowActivities {
 		driver.get("http://google.com");
 		driver.navigate().to("https://rahulshettyacademy.com");//navega desde la pagina anterior google, hacia rahulshettyacademy
 		driver.navigate().back();//navega hacia atras, vuelve a google, es como la flecha superior izquierda.
-		driver.navigate().forward();//navega hacia adelante nuevamente, desde la anterior google, hacia rahulshettyacademy.
+		driver.navigate().forward();//navega hacia adelante nuevamente, desde la anterior google, hacia rahulshettyacademy..
 
 	}
 
